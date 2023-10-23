e778c7f7fc8f2ae877d71ec4a893d68f859537c3
Gabriel <adrastopoulos@gmail.com>
2023-10-23 18:21:29 -0400

    make speakers reactive

--- app/speakers/page.tsx
+++ app/speakers/page.tsx
@@ -1,11 +1,15 @@
-import About from "@/components/about";
 import Image from "next/image";
 
 export default function Speakers() {
   return (
     <div>
-      <img src="/speakers.svg" alt="Speakers" className="w-2/3 mx-auto py-12" />
-      <About />
+      <Image
+        src="/speakers.svg"
+        alt="Speakers"
+        width={256}
+        height={256}
+        className="w-full max-w-4xl mx-auto py-12"
+      />
     </div>
   );
 }
