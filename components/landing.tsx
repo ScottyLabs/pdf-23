4fcb010c53cb29dc778e00468a408d05c474704b
Gabriel <adrastopoulos@gmail.com>
2023-10-23 20:13:38 -0400

    hide landing gif mobile

--- components/landing.tsx
+++ components/landing.tsx
@@ -6,7 +6,7 @@ export default function Landing() {
       <div className="mix-blend-multiply bg-primary-blue absolute rounded-full h-32 w-32 md:left-[25%] md:top-[60%] -top-[15%]" />
       <div className="mix-blend-multiply bg-primary-pink absolute rounded-b-full h-44 w-64 md:left-[25%] md:top-[40%] top-[85%]" />
       <div className="mix-blend-multiply bg-primary-yellow absolute rounded-full h-20 w-20 md:left-[25%] md:-top-[15%] left-[10%] -top-[20%]" />
-      <div className="absolute left-0 h-96 -top-10">
+      <div className="hidden md:block absolute left-0 h-96 -top-10">
         <Image
           height={128}
           width={128}
